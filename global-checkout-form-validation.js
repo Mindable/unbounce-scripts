075911e1f039d445aba488d85f0a2aa2bc5bcc07
Han Zhang <han@mindable.com>
2020-11-10 14:27:07 -0700

    global-checkout-form-validation - Use the pattern attribute of input elements to perform regex CC validation

--- global-checkout-form-validation.js
+++ global-checkout-form-validation.js
@@ -1,43 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   const form = document.getElementsByTagName('form')[0];
   if (!form) return;
-  const button = form.getElementsByTagName('button')[0];
-  if (!button) return;
-  button.addEventListener('click', validateForm);
-});
-
-function validateForm(event) {
-  const form = document.getElementsByTagName('form')[0];
-  if (!form) return true;
   const fieldsDiv = form.getElementsByClassName('fields')[0];
-  if (!fieldsDiv) return true;
-
-  const input_validator_map = {
-    'cc_number': input => {
-      console.log(`Validating: ${input.getAttribute('id')}`);
-    },
-    'cc_cvc': input => {
-      console.log(`Validating: ${input.getAttribute('id')}`);
-    }
-  };
-
-  let validationErrors = [];
-  // Note: This will only find <input>, not others like <select>
+  if (!fieldsDiv) return;
+  const input_pattern_map = {
+    // https://stackoverflow.com/a/9315696
+    'cc_number': '^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$',
+    'cc_cvc': '[0-9]{3,4}'
+  }
   let inputs = Array.from(fieldsDiv.getElementsByTagName('input'));
   inputs.forEach(input => {
-    let validator = input_validator_map[input.getAttribute('id')];
-    if (!validator) return true;
-    let validationError = validator(input);
-    if (validationError)
-      validationErrors.push(validationError);
+    let pattern = input_pattern_map[input.getAttribute('id')];
+    if (!pattern) return;
+    input.setAttribute('pattern', pattern);
   });
-
-  if (validationErrors.length > 0) return false;
-  return true;
-}
-
-// https://community.unbounce.com/t/how-to-run-custom-code-scripts-on-form-submission/5079
-window.ub.hooks.beforeFormSubmit.push(function (args) {
-  console.log('ub.hooks.beforeFormSubmit');
-  console.log(args);
 });
