3dbef3e4d037b32c87e6ab24da5da2f1f3d9f4f5
Han Zhang <han@mindable.com>
2020-11-10 16:57:08 -0700

    Recurly - load & configure recurly w/ public key

--- global-checkout-form-recurly.js
+++ global-checkout-form-recurly.js
@@ -1,17 +1,31 @@
+window.addEventListener('load', (event) => {
+  if (!useRecurly()) return;
+  
+  // Set PUBLIC key
+  recurly.configure('ewr1-TvB1JrfZgxptu9u8AtXhk8');
+  console.log('Recurly configured!');
+});
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  // TODO Check if form has hidden field declaring Recurly enabled
-  const form = document.getElementsByTagName('form')[0];
-  if (!form) return;
-
+  if (!useRecurly()) return;
+  
   // Reference recurly.js CDN assets
   // https://developers.recurly.com/reference/recurly-js/#getting-started
-  var scriptRef = document.createElement('script');
+  const scriptRef = document.createElement('script');
   scriptRef.src = 'https://js.recurly.com/v4/recurly.js';
   document.getElementsByTagName("head")[0].appendChild(scriptRef);
-  var cssRef = document.createElement('link');
+  const cssRef = document.createElement('link');
   cssRef.href = 'https://js.recurly.com/v4/recurly.css';
   cssRef.type = 'text/css';
   cssRef.rel = 'stylesheet';
   document.getElementsByTagName("head")[0].appendChild(cssRef);
 });
+
+function useRecurly() {
+  // Continue IFF the form has hidden field `_use_recurly`
+  const form = document.getElementsByTagName('form')[0];
+  if (!form) return false;
+  const useRecurly = Array.from(form.getElementsByTagName('input')).filter(e => e.id == '_use_recurly')[0];
+  if (!useRecurly) return false;
+  return true;
+}
