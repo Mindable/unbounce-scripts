417a3bdb2e28d7fb84d907c201cb3b95aace988f
Han Zhang <han@mindable.com>
2020-11-10 13:54:46 -0700

    Validate on formButton.Click

--- global-checkout-form-validation.js
+++ global-checkout-form-validation.js
@@ -1,26 +1,43 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  const input_validator_map = {
-    'cc_number': input => {
-      console.log(input.getAttribute('id'));
-    },
-    'cc_cvc': input => { }
-  };
-
   const form = document.getElementsByTagName('form')[0];
   if (!form) return;
+  const button = form.getElementsByTagName('button')[0];
+  if (!button) return;
+  button.addEventListener('click', validateForm);
+});
 
-  let fieldsDiv = form.getElementsByClassName('fields')[0];
-  if (!fieldsDiv) return;
+function validateForm(event) {
+  const form = document.getElementsByTagName('form')[0];
+  if (!form) return true;
+  const fieldsDiv = form.getElementsByClassName('fields')[0];
+  if (!fieldsDiv) return true;
 
+  const input_validator_map = {
+    'cc_number': input => {
+      console.log(`Validating: ${input.getAttribute('id')}`);
+    },
+    'cc_cvc': input => {
+      console.log(`Validating: ${input.getAttribute('id')}`);
+    }
+  };
+
+  let validationErrors = [];
   // Note: This will only find <input>, not others like <select>
   let inputs = Array.from(fieldsDiv.getElementsByTagName('input'));
-  console.log(inputs);
   inputs.forEach(input => {
-    // if (Object.keys(input_validator_map).includes(input.getAttribute('id'))) {
-    //   inv
-    // }
     let validator = input_validator_map[input.getAttribute('id')];
-    if (!validator) return;
-    validator(input);
+    if (!validator) return true;
+    let validationError = validator(input);
+    if (validationError)
+      validationErrors.push(validationError);
   });
+
+  if (validationErrors.length > 0) return false;
+  return true;
+}
+
+// https://community.unbounce.com/t/how-to-run-custom-code-scripts-on-form-submission/5079
+window.ub.hooks.beforeFormSubmit.push(function (args) {
+  console.log('ub.hooks.beforeFormSubmit');
+  console.log(args);
 });
