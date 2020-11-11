b1cdda36687c760a95bac17ffa64081c64c51a83
Han Zhang <han@mindable.com>
2020-11-10 17:16:28 -0700

    Recurly card element dynamic replace; ran into rendering issue again

--- global-checkout-form-recurly.js
+++ global-checkout-form-recurly.js
@@ -1,14 +1,29 @@
 window.addEventListener('load', (event) => {
   if (!useRecurly()) return;
-  
+
   // Set PUBLIC key
   recurly.configure('ewr1-TvB1JrfZgxptu9u8AtXhk8');
   console.log('Recurly configured!');
+
+  const form = document.getElementsByTagName('form')[0];
+  let fieldsDiv = form.getElementsByClassName('fields')[0];
+  if (!fieldsDiv) return;
+  var ccElement = Array.from(fieldsDiv.getElementsByTagName('input')).filter(e => e.id == 'cc_number')[0];
+  console.log('ccElement');
+  console.log(ccElement);
+  if (!ccElement) return;
+
+  var recurlyCardDiv = document.createElement('div');
+  recurlyCardDiv.id = 'cc_number';
+  ccElement.replaceWith(recurlyCardDiv);
+
+  const recurlyCardElement = recurly.Elements().CardElement();
+  recurlyCardElement.attach('#cc_number');
 });
 
 document.addEventListener('DOMContentLoaded', (event) => {
   if (!useRecurly()) return;
-  
+
   // Reference recurly.js CDN assets
   // https://developers.recurly.com/reference/recurly-js/#getting-started
   const scriptRef = document.createElement('script');
