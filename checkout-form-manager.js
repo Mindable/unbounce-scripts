993cba439d37df0c0bd9e0e6126610cc1fad1ea1
Han Zhang <han@mindable.com>
2020-11-27 14:52:24 -0700

    Add country list prefill; simplify user data prefill

--- checkout-form-manager.js
+++ checkout-form-manager.js
@@ -16,28 +16,24 @@ function getCheckoutFormBody(_config) {
     return "<form id='aa-checkout-form' method='POST'>" +
     "<h2><span style='font-family: lato; font-size: 24px; color: rgb(0, 0, 0); text-decoration: underline;'>Contact Details</span></h2>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='firstName' class='checkout-label'>First Name :</label>" +
-    "</div><div class='checkout-col'><input type='text' id='firstname' checkout-prefill='user/firstname' required></div></div>" +
+    "</div><div class='checkout-col'><input type='text' id='firstname' checkout-prefill='firstname' required></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='lastName' class='checkout-label'>Last Name :</label>" +
-    "</div><div class='checkout-col'><input type='text' id='lastname' checkout-prefill='user/lastname' required></div></div>" +
+    "</div><div class='checkout-col'><input type='text' id='lastname' checkout-prefill='lastname' required></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='emailId' class='checkout-label'>Email Address :</label>" +
-    "</div><div class='checkout-col'><input type='email' id='email' checkout-prefill='user/email' readonly required></div></div>" +
+    "</div><div class='checkout-col'><input type='email' id='email' checkout-prefill='email' readonly required></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='phone' class='checkout-label'>Contact Number :</label>" +
-    "</div><div class='checkout-col'><input type='text' id='phone' checkout-prefill='user/phone'></div></div>" +
+    "</div><div class='checkout-col'><input type='text' id='phone' checkout-prefill='phone'></div></div>" +
     "<br><br><h2><span style='font-family: lato; font-size: 24px; color: rgb(0, 0, 0); text-decoration: underline;'>Current Billing Address</span></h2>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='adr' class='checkout-label'>Address :</label>" +
-    "</div><div class='checkout-col'><input type='text' id='adr' checkout-prefill='user/address' required></div></div>" +
+    "</div><div class='checkout-col'><input type='text' id='adr' checkout-prefill='address' required></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='city' class='checkout-label'>City :</label>" +
-    "</div><div class='checkout-col'><input type='text' id='city' checkout-prefill='user/city'></div></div>" +
+    "</div><div class='checkout-col'><input type='text' id='city' checkout-prefill='city'></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='postal-code'' class='checkout-label'>Zip/Postal Code :</label>" +
-    "</div><div class='checkout-col'><input type='text' id='postal-code' checkout-prefill='user/postal-code' required></div></div>" +
+    "</div><div class='checkout-col'><input type='text' id='postal-code' checkout-prefill='postal-code' required></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='country' class='checkout-label'>Country :</label>" +
-    "</div><div class='checkout-col'><input id='country' checkout-prefill='user/country' list='countriesList'>" +
-    "<datalist id='countriesList'>" +
-    "</datalist></div></div>" +
+    "</div><div class='checkout-col'><select id='country' checkout-prefill='country'></select></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='state' class='checkout-label'>State/Province :</label>" +
-    "</div><div class='checkout-col'><input id='state' list='statesList'>" +
-    "<datalist id='statesList'>" +
-    "</datalist></div></div>" +
+    "</div><div class='checkout-col'><select id='state'></select></div></div>" +
     "<br><br><h2><span style='font-family: lato; font-size: 24px; color: rgb(0, 0, 0); text-decoration: underline;'>Credit Card Information</span></h2>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='cc_type' class='checkout-label'>Credit card Type :</label>" +
     "</div><div class='checkout-col'><select id='cc_type'><option value=0>Select One</option><option value='mastercard'>MasterCard</option><option value='visa'>VISA</option></select></div></div>" +
@@ -46,9 +42,9 @@ function getCheckoutFormBody(_config) {
     "<div class='checkout-row'><div class='checkout-col'><label for='cc_number' class='checkout-label'>CVV :</label>" +
     "</div><div class='checkout-col'><input type='text' id='cc_cvv' required minlength='3' maxlength='4'></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='cc_month' class='checkout-label'>Expiry Month: </label></div>" +
-    "<div class='checkout-col'><select id='cc_month' list='ccMonthList'></select></div></div>" +
+    "<div class='checkout-col'><select id='cc_month'></select></div></div>" +
     "<div class='checkout-row'><div class='checkout-col'><label for='cc_year' class='checkout-label'>Expiry Year: </label></div>" +
-    "<div class='checkout-col'><select id='cc_year' list='ccMonthList'></select></div></div>" +
+    "<div class='checkout-col'><select id='cc_year'></select></div></div>" +
     "<button class='lp-element lp-pom-button' type='submit'>" + _config.checkoutButtonText + "</button>" +
     "</form>";
 }
@@ -82,6 +78,12 @@ function setupForm(form) {
         option.value = year;
         ccYearSelect.appendChild(option);
     });
+
+    const countrySelect = form.querySelector('#country');
+    countrySelect.addEventListener('change', e => {
+        const stateSelect = form.querySelector('#state');
+        console.log('country changed');
+    });
 }
 
 function prefillForm(form) {
@@ -96,22 +98,27 @@ function prefillForm(form) {
                 return;
             }
             response.json().then(data => {
+                // Set input ranges for address->country
+                const countrySelect = form.querySelector('#country');
+                var countryData = data['address']['countries'];
+                console.log(countryData);
+                for (let key in countryData) {
+                    const option = document.createElement('option');
+                    option.text = countryData[key];
+                    option.value = key;
+                    countrySelect.appendChild(option);
+                }
+
+                // Prefill input values from user
+                const userData = data['user'];
+                if (!userData) return;
                 const inputs = Array.from(form.getElementsByTagName('input'));
                 inputs.forEach(input => {
-                    /**
-                     * Because the `data` is nested, we use `checkout-prefill` value as a \-separated key to navigated `data`
-                     * e.g. key 'user/firstname' -> {user: {firstname: Han}} -> Han
-                     */
                     const dataFieldKey = input.getAttribute('checkout-prefill');
                     if (!dataFieldKey) return;
-                    const keySegments = dataFieldKey.split('/');
-                    let value = data;
-                    while (keySegments.length > 0) {
-                        const segment = keySegments.shift();
-                        value = value[segment];
-                    }
+                    const value = userData[dataFieldKey];
                     if (!value || typeof(value) == 'object') return;
-                    input.value = value;
+                    input.value = value;                    
                 });
             });
         });
