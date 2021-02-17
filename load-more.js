4568a2024cb108786f6b813186d51927a42d45a3
Phillip Darch <pjdarch@gmail.com>
2021-02-17 15:11:56 -0700

    ES6 variable reassignment

--- load-more.js
+++ load-more.js
@@ -1,15 +1,13 @@
-'use strict';
-
 function registerLoadMoreClickEvent() {
-    var scripts = document.getElementsByTagName('script');
+    const scripts = document.getElementsByTagName('script');
     // Only register the click event for the latest instance of this script
     // This prevents an existing click event from being re-registered
-    for (var i = scripts.length - 1; i >= 0; i--) {
-        var script = scripts[i];
+    for (let i = scripts.length - 1; i >= 0; i--) {
+        const script = scripts[i];
         if (script.src.indexOf('load-more.js') !== -1) {
-            var trigger = script.getAttribute('data-trigger-id');
+            const trigger = script.getAttribute('data-trigger-id');
             if (trigger) {
-                var loadMoreElement = document.getElementById(trigger);
+                let loadMoreElement = document.getElementById(trigger);
                 if (loadMoreElement) {
                     loadMoreElement.addEventListener('click', function (event) {
                         event.preventDefault();
@@ -23,11 +21,11 @@ function registerLoadMoreClickEvent() {
 }
 
 function renderPageAndScroll(loadMoreElement) {
-    var href = loadMoreElement.getAttribute('href');
+    const href = loadMoreElement.getAttribute('href');
     if (href) {
-        var div = document.createElement('div');
+        let div = document.createElement('div');
         document.body.appendChild(div);
-        var xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
 
         xhr.onload = function () {
             div.innerHTML = this.response;
@@ -36,7 +34,7 @@ function renderPageAndScroll(loadMoreElement) {
         xhr.onreadystatechange = function () {
             // In local files, status is 0 upon success in Mozilla Firefox
             if (xhr.readyState === XMLHttpRequest.DONE) {
-                var status = xhr.status;
+                const status = xhr.status;
                 if (status === 0 || (status >= 200 && status < 400)) {
                     // We need to use a timeout to delay execution, so as to allow the newly added content time to finish rendering
                     setTimeout(function () {
