d9821f91fc6461e0fd391e6fddc3cd57e13c86d4
Gaurav Bains <42105996+gaurav-bains@users.noreply.github.com>
2021-05-27 16:09:47 -0600

    Fixed Product Summary Design (#50)
    * Fixed Product Summary Design
    
    * Removed Product prefix from Product Name

--- dist/checkout/checkout.js
+++ dist/checkout/checkout.js
@@ -351,10 +351,10 @@ app.component('product-pricing', {
   },
   template: `<h3>Product Information</h3>
   <div>
-  <p>Product : {{ productVariant.name }}</p>
-  <p v-if="taxPrc > 0">Price : $ {{productVariant.price}}</p>
-  <p v-if="taxPrc > 0">Tax ({{taxPrc}}%) : $ {{tax}}</p>
-  <p>Total : $ {{amount}}</p>
+  <p><strong>Product : {{ productVariant.name }}</strong></p>
+  <p v-if="taxPrc > 0"><strong>Price : $ {{productVariant.price}}</strong></p>
+  <p v-if="taxPrc > 0"><strong>Tax ({{taxPrc}}%) : $ {{tax}}</strong></p>
+  <p><strong>Total : $ {{amount}}</strong></p>
   </div>`,
   data() {
     return {
